8ad3b5696228402023af7a9de0b793f15a035002
Ilya Lugovoy <ilya.l2013@yandex.ru>
2022-04-15 00:20:23 +0300

    Lab2 MultiActivity

---
+++ Lab2/multiactivity/src/main/java/ru/mirea/lugovoy/practice2/multiactivity/SecondActivity.java
@@ -0,0 +1,112 @@
+package ru.mirea.lugovoy.practice2.multiactivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity
+{
+    private final String TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Log.i(TAG,"onCreate()");
+
+        String text = (String) getIntent().getSerializableExtra("key");
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(TAG,"onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG,"onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        Log.i(TAG,"onPostCreate()");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPostResume()
+    {
+        super.onPostResume();
+        Log.i(TAG,"onPostResume()");
+    }
+
+    @Override
+    public void onAttachedToWindow()
+    {
+        super.onAttachedToWindow();
+        Log.i(TAG,"onAttachedToWindow()");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG,"onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.i(TAG,"onStop()");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy()");
+    }
+
+    @Override
+    public void onDetachedFromWindow()
+    {
+        super.onDetachedFromWindow();
+        Log.i(TAG,"onDetachedFromWindow()");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
+    }
+}
