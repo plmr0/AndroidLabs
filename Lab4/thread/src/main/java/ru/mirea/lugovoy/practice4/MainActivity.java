cded3cd4c419f3963fbf3fb29ead6a25d409b968
Ilya Lugovoy <ilya.l2013@yandex.ru>
2022-04-22 21:07:53 +0300

    Lab4 Thread

---
+++ Lab4/thread/src/main/java/ru/mirea/lugovoy/practice4/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.mirea.lugovoy.practice4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    int counter = 0;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView infoTextView = findViewById(R.id.textView);
+        Thread mainThread = Thread.currentThread();
+        infoTextView.setText("Текущий поток: " + mainThread.getName());
+
+        // Меняем имя и выводим в текстовом поле
+        mainThread.setName("MireaThread");
+        infoTextView.append("\nНовое имя потока: " + mainThread.getName());
+    }
+
+    public void onClick(View view)
+    {
+        Runnable runnable = new Runnable()
+        {
+            public void run()
+            {
+                int numberThread = counter++;
+                Log.i("ThreadProject", "Запущен поток № " + numberThread);
+                long endTime = System.currentTimeMillis() + 20 * 1000;
+
+                while (System.currentTimeMillis() < endTime)
+                {
+                    synchronized (this)
+                    {
+                        try
+                        {
+                            wait(endTime - System.currentTimeMillis());
+                        }
+                        catch (Exception ignored) { }
+                    }
+                }
+                Log.i("ThreadProject", "Выполнен поток № " + numberThread);
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
