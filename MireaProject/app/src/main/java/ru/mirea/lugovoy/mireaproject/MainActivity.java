b068274117bb1d61fac1097e7fc70c3c7b2ff56a
Ilya Lugovoy <ilya.l2013@yandex.ru>
2022-04-29 01:05:14 +0300

    MireaProject SharedPreferences

--- MireaProject/app/src/main/java/ru/mirea/lugovoy/mireaproject/MainActivity.java
+++ MireaProject/app/src/main/java/ru/mirea/lugovoy/mireaproject/MainActivity.java
@@ -1,8 +1,13 @@
 package ru.mirea.lugovoy.mireaproject;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,6 +25,17 @@ public class MainActivity extends AppCompatActivity
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    private final String SAVED_NAME = "saved_name";
+    private final String SAVED_GROUP = "saved_group";
+
+    private TextView navName;
+    private TextView navGroup;
+
+    private NavigationView navigationView;
+    private View headerView;
+
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,7 +47,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(binding.appBarMain.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -41,12 +57,28 @@ public class MainActivity extends AppCompatActivity
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        this.headerView = navigationView.getHeaderView(0);
+
+        this.navName = (TextView) this.headerView.findViewById(R.id.headerAuthorName);
+        this.navGroup = (TextView) this.headerView.findViewById(R.id.headerAuthorGroup);
+
+        load();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 228 && resultCode == RESULT_OK)
+        {
+            String name = data.getExtras().getString("name", getStringRes(R.string.nav_header_title));
+            String group = data.getExtras().getString("group", getStringRes(R.string.nav_header_subtitle));
+
+            this.navName.setText(name);
+            this.navGroup.setText(group);
+        }
     }
 
     @Override
@@ -62,5 +94,39 @@ public class MainActivity extends AppCompatActivity
     {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.action_settings)
+        {
+            openSettings();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String getStringRes(int id)
+    {
+        return getResources().getString(id);
+    }
+
+    private void load()
+    {
+        this.preferences = getSharedPreferences("user", MODE_PRIVATE);
+
+        String name = this.preferences.getString(SAVED_NAME, getStringRes(R.string.nav_header_title));
+        String group = this.preferences.getString(SAVED_GROUP, getStringRes(R.string.nav_header_subtitle));
+
+        this.navName.setText(name);
+        this.navGroup.setText(group);
+    }
+
+    private void openSettings()
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivityForResult(intent, 228);
     }
 }
---
+++ MireaProject/app/src/main/java/ru/mirea/lugovoy/mireaproject/SettingsActivity.java
@@ -0,0 +1,96 @@
+package ru.mirea.lugovoy.mireaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    private EditText nameEditText;
+    private EditText groupEditText;
+
+    private Button saveButton;
+
+    private final String SAVED_NAME = "saved_name";
+    private final String SAVED_GROUP = "saved_group";
+
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        this.nameEditText = (EditText) findViewById(R.id.nameEditText);
+        this.groupEditText = (EditText) findViewById(R.id.groupEditText);
+
+        this.saveButton = (Button) findViewById(R.id.saveSettingsButton);
+
+        this.saveButton.setOnClickListener(this::onClickSave);
+
+        this.preferences = getSharedPreferences("user", MODE_PRIVATE);
+
+        load();
+    }
+
+    private String getStringRes(int id)
+    {
+        return getResources().getString(id);
+    }
+
+    private void load()
+    {
+        String name = this.preferences.getString(SAVED_NAME, "");
+        String group = this.preferences.getString(SAVED_GROUP, "");
+
+        if (name.equals(getStringRes(R.string.nav_header_subtitle)))
+        {
+            name = "";
+        }
+
+        if (group.equals(getStringRes(R.string.nav_header_title)))
+        {
+            group = "";
+        }
+
+        this.nameEditText.setText(name);
+        this.groupEditText.setText(group);
+    }
+
+    private void onClickSave(View view)
+    {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        String name = this.nameEditText.getText().toString();
+        String group = this.groupEditText.getText().toString();
+
+        if (name.length() == 0)
+        {
+            name = getStringRes(R.string.nav_header_subtitle);
+        }
+
+        if (group.length() == 0)
+        {
+            group = getStringRes(R.string.nav_header_title);
+        }
+
+        editor.putString(SAVED_NAME, name);
+        editor.putString(SAVED_GROUP, group);
+
+        editor.apply();
+
+        Intent resultIntent = new Intent();
+
+        resultIntent.putExtra("name", name);
+        resultIntent.putExtra("group", group);
+
+        setResult(RESULT_OK, resultIntent);
+        finish();
+    }
+}
